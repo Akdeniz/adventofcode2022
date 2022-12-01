9121d9d157bb7efbfa3f9123c5f35376510d6d82
Ali Demiroz <demirozali@gmail.com>
2022-12-01 21:35:52 +0000

    day1 cpp

---
+++ day01/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+int main() {
+
+    std::fstream ifs("/Users/ecem/CLionProjects/adventofcode2022/day01/day1.txt");
+    std::string line;
+
+    std::vector<int64_t> sums = {0};
+    while(std::getline(ifs, line))
+    {
+        if(line.empty())
+            sums.push_back(0);
+        else
+            sums.back()+=std::stoll(line);
+    }
+
+    std::sort(sums.begin(), sums.end(), std::greater<int64_t>());
+
+    std::cout << "Part1: " << sums.front() << std::endl;
+    std::cout << "Part2: " << std::accumulate(sums.begin(), sums.begin()+std::min<int>(3, sums.size()), 0) << std::endl;
+    return 0;
+}
