9bb32a059be91a2625c94d1934eba93655561ce4
Ali Demiroz <demirozali@gmail.com>
2022-12-02 22:45:18 +0000

    day2

---
+++ day02/main.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+enum class Move
+{
+    Rock = 0,
+    Paper,
+    Scissors
+};
+
+struct Turn
+{
+    Move op, you;
+};
+
+auto read()
+{
+    std::fstream ifs("/Users/ecem/CLionProjects/adventofcode2022/day02/day2.txt");
+    std::string line;
+    std::vector<Turn> result;
+    while(std::getline(ifs, line))
+    {
+        if(line.empty())
+            continue;
+
+        // A for Rock, B for Paper, and C for Scissors
+        // X for Rock, Y for Paper, and Z for Scissors
+        result.push_back({static_cast<const Move>(line[0] - 'A'), static_cast<const Move>(line[2] - 'X')});
+    }
+    return result;
+}
+
+int score(Move op, Move you)
+{
+    // Rock defeats Scissors, Scissors defeats Paper, and Paper defeats Rock.
+    // If both players choose the same shape, the round instead ends in a draw.
+    int res = 0;
+    if(op == you)
+        res = 3;
+    else if((you == Move::Rock && op == Move::Scissors) || (you == Move::Scissors && op == Move::Paper) || (you == Move::Paper && op == Move::Rock))
+        res = 6;
+
+    // 1 for Rock, 2 for Paper, and 3 for Scissors) plus
+    // the res for the outcome of the round (0 if you lost, 3 if the round was a draw, and 6 if you won)
+    return res + static_cast<int>(you) + 1;
+}
+
+void part1()
+{
+
+    int64_t result = 0;
+    for(const auto& turn: read())
+        result+=score(turn.op, turn.you);
+
+    std::cout << "Part1: " << result << std::endl;
+}
+
+void part2()
+{
+    static std::vector<std::vector<Move>> table = {
+            // lose, draw, win
+            {Move::Scissors, Move::Rock,     Move::Paper}, // Rock
+            {Move::Rock,     Move::Paper,    Move::Scissors,}, // Paper
+            {Move::Paper,    Move::Scissors, Move::Rock}, // Scissors
+    };
+
+    int64_t result = 0;
+    for(const auto& turn: read())
+    {
+        const auto you = table[static_cast<int>(turn.op)][static_cast<int>(turn.you)];
+        result+= score(turn.op, you);
+    }
+
+    std::cout << "Part2: " << result << std::endl;
+}
+
+int main() {
+
+    part1();
+    part2();
+    return 0;
+}
---
+++ day02/src/main.rs
@@ -0,0 +1,90 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+use std::convert::TryFrom;
+
+#[derive(Copy, Clone, PartialEq)]
+enum Move { R=0, P=1, S=2}
+
+impl TryFrom<i32> for Move {
+    type Error = ();
+
+    fn try_from(v: i32) -> Result<Self, Self::Error> {
+        match v {
+            x if x == Move::R as i32 => Ok(Move::R),
+            x if x == Move::P as i32 => Ok(Move::P),
+            x if x == Move::S as i32 => Ok(Move::S),
+            _ => Err(()),
+        }
+    }
+}
+
+impl Move {
+    pub fn index(&self) -> usize {
+        *self as usize
+    }
+}
+
+fn parse(s: String) -> (Move, Move) {
+    let op_digit = s.as_bytes()[0] as i32;
+    let op =  Move::try_from(op_digit-'A' as i32).unwrap();
+
+    let you_digit = s.as_bytes()[2] as i32;
+    let you = Move::try_from(you_digit-'X' as i32).unwrap();
+    return (op, you)
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> where P: AsRef<Path> {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn read_moves() -> Vec<(Move, Move)> {
+    let file_path = "/Users/ecem/CLionProjects/adventofcode2022/day02/day2.txt";
+    let mut moves : Vec<(Move, Move)> = vec!();
+    let Ok(lines) = read_lines(file_path) else {
+        return moves;
+    };
+
+    for line in lines {
+        moves.push(parse(line.unwrap()));
+    }
+    return moves;
+}
+
+fn score(op: Move, you:Move) -> i32 {
+    let mut res = 0;
+    if op == you {
+        res = 3;
+    } else if you==Move::R && op==Move::S || you==Move::S && op==Move::P || you==Move::P && op==Move::R {
+        res = 6;
+    }
+    return res + (you as i32) + 1;
+}
+
+fn part1() {
+    let result :i32 = read_moves().into_iter().map(|m| score(m.0, m.1)).sum();
+
+    println!("Part1: {}", result)
+}
+
+fn part2() {
+    let table = vec![
+        [Move::S, Move::R, Move::P], // R
+        [Move::R, Move::P, Move::S], // P
+        [Move::P, Move::S, Move::R]  // S
+    ];
+
+    let result :i32 = read_moves().into_iter().map(|m| {
+        score(m.0, table[m.0.index()][m.1.index()])
+    }).sum();
+
+    println!("Part2: {}", result)
+}
+
+fn main() {
+    part1();
+    part2();
+}
+
+
