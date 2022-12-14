19404d7f5155a0f87f18ea2f7d2a009a4f97e35b
Ali Demiroz <demirozali@gmail.com>
2022-12-14 23:10:42 +0000

    day14 rust

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,6 +1,6 @@
 use std::cell::RefCell;
 use std::cmp::Ordering;
-use std::collections::{HashMap, VecDeque};
+use std::collections::{HashMap, VecDeque, HashSet};
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::ops::Deref;
@@ -9,6 +9,7 @@ use std::rc::Rc;
 use std::vec;
 use regex::Regex;
 use scanf::sscanf;
+use lazy_static::lazy_static;
 
 fn read_all() -> String {
     let file_path = "input.txt";
@@ -19,102 +20,140 @@ fn read_lines() -> Vec<String> {
     read_all().split('\n').map(|x| x.parse().unwrap()).collect::<Vec<String>>()
 }
 
-struct Node {
-    pos: (usize, usize),
-    distance: usize,
+fn parse(input: &str) -> Vec<(i32,i32)> {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r#"(?P<y>[0-9]+),(?P<x>[0-9]+)"#).unwrap();
+    }
+
+    let mut result: Vec<(i32,i32)> = vec![];
+    for cap in RE.captures_iter(input) {
+
+        let x = cap.name("x").unwrap().parse::<i32>().unwrap();
+        let y = cap.name("y").unwrap().parse::<i32>().unwrap();
+        result.push((x,y));
+    }
+    result
 }
 
-fn distance(table: &Vec<&[u8]>, mut q: Vec<Node>, dest: (usize, usize)) -> Option<usize> {
-    let nexts: Vec<(i32, i32)> = vec![(-1, 0), (0, 1), (1, 0), (0, -1)];
+fn drop_sand(walls: &mut HashSet<(i32, i32)>, lowest: i32) -> bool {
+    let mut sand = (0,500);
 
-    let mut visited :HashMap<(usize, usize), usize> = HashMap::new();
-    loop {
-        if q.is_empty() {
-            break;
+    while sand.0 <= lowest {
+        if !walls.contains(&(sand.0 + 1, sand.1)) {
+            sand = (sand.0 + 1, sand.1);
+        } else if !walls.contains(&(sand.0 + 1, sand.1 - 1)) {
+            sand = (sand.0 + 1, sand.1 - 1);
+        } else if !walls.contains(&(sand.0 + 1, sand.1 + 1)) {
+            sand = (sand.0 + 1, sand.1 + 1);
+        } else {
+            walls.insert(sand);
+            return true;
         }
-
-        let mut newq: Vec<Node> = vec![];
-        for i in 0..q.len() {
-            let node = &q[i];
-
-            if let Some(value) = visited.get(&node.pos) {
-                if value <= &node.distance {
-                    continue;
-                }
-            }
-
-            visited.insert(node.pos, node.distance);
-
-            if node.pos == dest {
-                return Some(node.distance);
-            }
-
-            for next in &nexts {
-                let newx = next.0 + (q[i].pos.0 as i32);
-                let newy = next.1 + (q[i].pos.1 as i32);
-                if newx < 0 || newx >= table.len() as i32 || newy < 0 || newy >= table[0].len() as i32 {
-                    continue;
-                }
-
-                if table[newx as usize][newy as usize] > table[node.pos.0][node.pos.1] + 1 {
-                    continue;
-                }
-                newq.push(Node { pos: (newx as usize, newy as usize), distance: node.distance + 1 });
-            }
-        }
-        q = newq;
     }
-    None
+    false
 }
 
 fn part1() {
-    let mut lines = read_lines().into_iter().filter(|x| !x.is_empty()).collect::<Vec<String>>();
+    let lines = read_lines().into_iter().filter(|x| !x.is_empty()).collect::<Vec<String>>();
 
-    let mut start = (0, 0);
-    let mut end = (0, 0);
-    for i in 0..lines.len() {
-        let bytes = lines[i].as_bytes();
-        for j in 0..bytes.len() {
-            if bytes[j] as char == 'S' {
-                start = (i, j);
+    let mut walls :HashSet<(i32, i32)> = HashSet::new();
+    for line in lines {
+        let positions = parse(line.as_str());
+        for i in 1..positions.len() {
+            let deltax = match positions[i-1].0.cmp(&positions[i].0) {
+                Ordering::Less => 1,
+                Ordering::Equal => 0,
+                Ordering::Greater => -1
+            };
+            let deltay = match positions[i-1].1.cmp(&positions[i].1) {
+                Ordering::Less => 1,
+                Ordering::Equal => 0,
+                Ordering::Greater => -1
+            };
+
+            let mut from = positions[i-1];
+            while from!=positions[i] {
+                walls.insert(from);
+                from = (from.0+deltax, from.1+deltay);
             }
-            if bytes[j] as char == 'E' {
-                end = (i, j);
-            }
+            walls.insert(positions[i]);
         }
-        lines[i] = lines[i].replace("S", "a").replace("E", "z");
     }
 
-    let table = &lines.iter().map(|x| x.as_bytes()).collect::<Vec<&[u8]>>();
+    let lowest = walls.iter().map(|x| x.0).reduce(|x,y| x.max(y)).unwrap();
 
-    let result = distance(table, vec![Node { pos: start, distance: 0 }], end);
-    println!("Part1: {}", result.unwrap())
+    let mut result = 0;
+    while drop_sand(&mut walls, lowest) {
+        result+=1;
+    }
+
+    println!("Part1: {}", result);
+}
+
+fn drop_sand_2(walls: &mut HashSet<(i32, i32)>, base: i32) -> bool {
+    let mut sand = (0,500);
+
+    if walls.contains(&sand) {
+        return false;
+    }
+
+    while true {
+
+        if base == sand.0 + 1 {
+            walls.insert(sand);
+            return true;
+        }
+
+        if !walls.contains(&(sand.0 + 1, sand.1)) {
+            sand = (sand.0 + 1, sand.1);
+        } else if !walls.contains(&(sand.0 + 1, sand.1 - 1)) {
+            sand = (sand.0 + 1, sand.1 - 1);
+        } else if !walls.contains(&(sand.0 + 1, sand.1 + 1)) {
+            sand = (sand.0 + 1, sand.1 + 1);
+        } else {
+            walls.insert(sand);
+            return true;
+        }
+    }
+    false
 }
 
 fn part2() {
-    let mut lines = read_lines().into_iter().filter(|x| !x.is_empty()).collect::<Vec<String>>();
+    let lines = read_lines().into_iter().filter(|x| !x.is_empty()).collect::<Vec<String>>();
 
-    let mut starting_points: Vec<Node> = vec![];
-    let mut end = (0, 0);
-    for i in 0..lines.len() {
-        let bytes = lines[i].as_bytes();
-        for j in 0..bytes.len() {
-            if bytes[j] as char == 'S' || bytes[j] as char == 'a' {
-                starting_points.push(Node{pos: (i, j), distance:0});
+    let mut walls :HashSet<(i32, i32)> = HashSet::new();
+    for line in lines {
+        let positions = parse(line.as_str());
+        for i in 1..positions.len() {
+            let deltax = match positions[i-1].0.cmp(&positions[i].0) {
+                Ordering::Less => 1,
+                Ordering::Equal => 0,
+                Ordering::Greater => -1
+            };
+            let deltay = match positions[i-1].1.cmp(&positions[i].1) {
+                Ordering::Less => 1,
+                Ordering::Equal => 0,
+                Ordering::Greater => -1
+            };
+
+            let mut from = positions[i-1];
+            while from!=positions[i] {
+                walls.insert(from);
+                from = (from.0+deltax, from.1+deltay);
             }
-            if bytes[j] as char == 'E' {
-                end = (i, j);
-            }
+            walls.insert(positions[i]);
         }
-        lines[i] = lines[i].replace("S", "a").replace("E", "z");
     }
 
-    let table = &lines.iter().map(|x| x.as_bytes()).collect::<Vec<&[u8]>>();
+    let lowest = walls.iter().map(|x| x.0).reduce(|x,y| x.max(y)).unwrap();
 
-    let result = distance(table, starting_points, end);
-    println!("Part2: {}", result.unwrap())
+    let mut result = 0;
+    while drop_sand_2(&mut walls, lowest+2) {
+        result+=1;
+    }
+
+    println!("Part2: {}", result);
 }
-
 
 fn main() {
     part1();
